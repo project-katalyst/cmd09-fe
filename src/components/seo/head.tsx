6ecccd0535a16b04321ff48c1a12c9b596f70b5b
Flavio Murata <flaviomurata02@gmail.com>
2025-07-25 18:26:32 -0300

    chore(home): add input and button components

--- src/components/seo/head.tsx
+++ src/components/seo/head.tsx
@@ -11,8 +11,8 @@ export const Head = ({ title = '', description = '' }: HeadProps = {}) => {
   return (
     <Helmet
       helmetData={helmetData}
-      title={title ? `${title} | Bulletproof React` : undefined}
-      defaultTitle="Bulletproof React"
+      title={title ? `${title} | Katalyst` : undefined}
+      defaultTitle="Katalyst"
     >
       <meta name="description" content={description} />
     </Helmet>
