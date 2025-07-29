17c652f3f49d392bed7bc9d105610aa6347f48bf
Flavio Murata <flaviomurata02@gmail.com>
2025-07-29 20:06:37 -0300

    feat(ranking): add animated-list ui component

--- src/app/routes/app/ranking.tsx
+++ src/app/routes/app/ranking.tsx
@@ -1,11 +1,30 @@
 import { ContentLayout } from '@/components/layouts/content-layout';
+import AnimatedList from '@/features/ranking/components/animated-list';
 
 const RankingRoute = () => {
+  const items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+    'Item6',
+    'Item 7',
+    'Item 8',
+    'Item 9',
+    'Item 10',
+  ];
   return (
     <ContentLayout title="Ranking">
-      <div className="mt-4">
-        {/* Render your ranking data here */}
+      <div className="mt-4 flex flex-col items-center gap-8">
         <h1>Ranking Page</h1>
+        <AnimatedList
+          items={items}
+          onItemSelect={(item, index) => console.log(item, index)}
+          showGradients={false}
+          enableArrowNavigation={true}
+          displayScrollbar={true}
+        />
       </div>
     </ContentLayout>
   );
