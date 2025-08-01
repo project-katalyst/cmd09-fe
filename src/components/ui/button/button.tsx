2cb5a510173a8a8ec39ea2bd892ad87bc4c69485
Flavio Murata <flaviomurata02@gmail.com>
2025-07-31 21:47:11 -0300

    style(ui): change button and input components styling

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -7,7 +7,7 @@ import { cn } from '@/lib/utils';
 import { Spinner } from '../spinner';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  'group relative w-full overflow-hidden rounded-md disabled:cursor-not-allowed disabled:opacity-50',
   {
     variants: {
       variant: {
@@ -23,9 +23,9 @@ const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-9 px-4 py-2',
+        default: 'px-6 py-3',
         sm: 'h-8 rounded-md px-3 text-xs',
-        lg: 'h-10 rounded-md px-8',
+        lg: 'h-12 rounded-md px-8 text-base',
         icon: 'size-9',
       },
     },
@@ -67,7 +67,9 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       >
         {isLoading && <Spinner size="sm" className="text-current" />}
         {!isLoading && icon && <span className="mr-2">{icon}</span>}
-        <span className="mx-2">{children}</span>
+        <span className="relative z-20 flex items-center justify-center gap-2">
+          {children}
+        </span>
       </Comp>
     );
   },
