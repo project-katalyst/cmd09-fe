de0fbe53f8a0f4e1afc6df495d040e2436303b4d
Flavio Murata <flaviomurata02@gmail.com>
2025-08-02 17:45:21 -0300

    style(layout): update content layout

--- src/app/routes/app/home.tsx
+++ src/app/routes/app/home.tsx
@@ -22,62 +22,60 @@ const HomeRoute = () => {
   return (
     <>
       <ContentLayout title="Home">
-        <div className="relative mx-auto flex min-h-screen w-full max-w-[1920px] flex-col items-center justify-center overflow-hidden md:px-16">
-          <div className="flex w-full max-w-4xl flex-col items-center gap-8">
-            <h1 className="text-h1 w-full text-center font-bold tracking-tight">
-              Descubra quem está interessado na sua empresa!
-            </h1>
-            <p className="text-title2 mx-auto text-balance text-center">
-              Descubra clientes e parceiros que já demonstram interesse no seu
-              negócio. Insira o nome da sua empresa e explore as oportunidades.
-            </p>
-          </div>
-          <div className="h-[160px] w-full md:h-[220px]">
-            <div className="flex size-full flex-col items-center justify-center">
-              <form
-                className="w-full max-w-md"
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  createRankingMutation.mutate({ data: { url } });
-                }}
-              >
-                <div className="flex flex-col gap-4">
-                  <Input
-                    type="text"
-                    placeholder="Digite a URL da sua empresa"
-                    value={url}
-                    onChange={(e) => setUrl(e.target.value)}
-                  />
-                  <Button
-                    type="submit"
-                    isLoading={createRankingMutation.isPending}
-                  >
-                    Buscar
-                  </Button>
-                </div>
-              </form>
-            </div>
-          </div>
-          <CardSwap
-            cardDistance={60}
-            verticalDistance={70}
-            delay={8000}
-            pauseOnHover={true}
-          >
-            <Card>
-              <h3>Card 1</h3>
-              <p>Your content here</p>
-            </Card>
-            <Card>
-              <h3>Card 2</h3>
-              <p>Your content here</p>
-            </Card>
-            <Card>
-              <h3>Card 3</h3>
-              <p>Your content here</p>
-            </Card>
-          </CardSwap>
+        <div className="flex w-full max-w-4xl flex-col items-center gap-8">
+          <h1 className="text-h1 w-full text-center font-bold tracking-tight">
+            Descubra quem está interessado na sua empresa!
+          </h1>
+          <p className="text-title2 mx-auto text-balance text-center">
+            Descubra clientes e parceiros que já demonstram interesse no seu
+            negócio. Insira o nome da sua empresa e explore as oportunidades.
+          </p>
         </div>
+        <div className="h-[160px] w-full md:h-[220px]">
+          <div className="flex size-full flex-col items-center justify-center">
+            <form
+              className="w-full max-w-md"
+              onSubmit={(e) => {
+                e.preventDefault();
+                createRankingMutation.mutate({ data: { url } });
+              }}
+            >
+              <div className="flex flex-col gap-4">
+                <Input
+                  type="text"
+                  placeholder="Digite a URL da sua empresa"
+                  value={url}
+                  onChange={(e) => setUrl(e.target.value)}
+                />
+                <Button
+                  type="submit"
+                  isLoading={createRankingMutation.isPending}
+                >
+                  Buscar
+                </Button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <CardSwap
+          cardDistance={60}
+          verticalDistance={70}
+          delay={8000}
+          pauseOnHover={true}
+        >
+          <Card>
+            <h3>Card 1</h3>
+            <p>Your content here</p>
+          </Card>
+          <Card>
+            <h3>Card 2</h3>
+            <p>Your content here</p>
+          </Card>
+          <Card>
+            <h3>Card 3</h3>
+            <p>Your content here</p>
+          </Card>
+        </CardSwap>
       </ContentLayout>
     </>
   );
--- src/components/layouts/content-layout.tsx
+++ src/components/layouts/content-layout.tsx
@@ -15,7 +15,9 @@ export const ContentLayout = ({ children, title }: ContentLayoutProps) => {
         <div className="mx-auto grid w-full max-w-[1920px] grid-cols-12 gap-2 self-center px-6 md:gap-4 md:px-10 lg:px-12 xl:gap-6 xl:px-20">
           <div className="col-span-12">
             <div className="flex max-w-full flex-col items-center">
-              {children}
+              <div className="relative mx-auto flex min-h-screen w-full max-w-[1920px] flex-col items-center justify-center overflow-hidden md:px-16">
+                {children}
+              </div>
             </div>
           </div>
         </div>
