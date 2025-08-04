e07ba1a55d966296509d8d317159b486b5ec3f8e
Flavio Murata <flaviomurata02@gmail.com>
2025-08-03 22:55:35 -0300

    feat(ui): add navbar and footer

---
+++ src/components/ui/footer/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="fixed bottom-0 left-0 inline-flex items-center gap-8 p-16 font-radar text-3xl">
+      <span>20</span>
+      <span className="h-1 w-80 bg-foreground"></span>
+      <span>25</span>
+    </div>
+  );
+};
+
+export { Footer };
